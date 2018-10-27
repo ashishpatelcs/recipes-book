9188903180d07239253575964899c167f7a643fe
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-27 22:56:21 +0530

    update recipe book structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +20,9 @@ import { RecipeBookModule } from './recipe-book/recipe-book.module';
     ]),
     BrowserModule,
     ShoppingListModule,
-    RecipeBookModule
+    RecipeBookModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RecipeListComponent, RecipeItemComponent, RecipeDetailComponent]
+  declarations: [RecipeListComponent, RecipeItemComponent, RecipeDetailComponent, RecipesComponent]
 })
 export class RecipeBookModule { }
