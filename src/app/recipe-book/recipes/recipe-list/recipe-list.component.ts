e0e75a73a368a806ab539c9f54a4a9fbfb4eac2d
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-28 00:15:05 +0530

    implement recipe list component

--- src/app/recipe-book/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test recipe', 'http://www.indianfoodforever.com/images/rainy-recipes.jpg'),
+    new Recipe('Paratha', 'This is paratha recipe',
+    'https://media.gettyimages.com/photos/indian-breakfast-in-goa-picture-id511910426?s=612x612')
+  ];
 
   constructor() { }
 
