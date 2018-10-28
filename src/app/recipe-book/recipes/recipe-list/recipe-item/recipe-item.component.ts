e0a8648d78993c8ddaceda1b8390ac5b52c25303
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-28 22:41:21 +0530

    update recipes comp

--- src/app/recipe-book/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,10 +9,15 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
   @Input('recipeItem') recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter <Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipe() {
+    this.recipeSelected.emit(this.recipe);
   }
 
 }
