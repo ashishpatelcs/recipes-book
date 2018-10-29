77e9b1e3e87456497934bce31bd9578af28d1199
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-29 06:07:12 +0530

    add appDropdowown to header component

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,8 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef, Renderer2, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
-  constructor() { }
+export class DropdownDirective implements OnInit, OnChanges {
+  @Input() openDropdown = false;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.openDropdown) {
+      this.renderer.addClass(this.elementRef.nativeElement, 'open');
+    } else {
+      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+    }
+  }
+
 }
